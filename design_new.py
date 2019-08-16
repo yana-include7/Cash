2c1f97ef5ead1c2ddcdf34bbcfca108d5e5c5c9b
yana-include7 <yana.include7@gmail.com>
2019-08-16 16:53:19 +0300

    Initial commit.

---
+++ design_new.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'design_new.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(574, 437)
+        MainWindow.setStyleSheet("background-color: rgb(255, 255, 255)")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setStyleSheet("font: 14pt \"Microsoft YaHei\";")
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setStyleSheet("font: 18pt \"Microsoft YaHei\";")
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout.addWidget(self.label_2)
+        self.lcdNumber = QtWidgets.QLCDNumber(self.centralwidget)
+        self.lcdNumber.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.lcdNumber.setStyleSheet("font: 10pt \"Microsoft YaHei\";")
+        self.lcdNumber.setObjectName("lcdNumber")
+        self.horizontalLayout.addWidget(self.lcdNumber)
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setStyleSheet("font: 18pt \"Microsoft YaHei\";")
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout.addWidget(self.label_3)
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setStyleSheet("font: 14pt \"Microsoft YaHei\";")
+        self.comboBox.setObjectName("comboBox")
+        self.horizontalLayout.addWidget(self.comboBox)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setStyleSheet("color: rgb(255, 47, 11);\n"
+"font: 18pt \"Microsoft YaHei\";")
+        self.label_6.setText("")
+        self.label_6.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_6.setObjectName("label_6")
+        self.verticalLayout.addWidget(self.label_6)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setStyleSheet("font: 14pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 214, 143);")
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout_2.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setStyleSheet("font: 14pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 214, 143);")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout_2.addWidget(self.pushButton_2)
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setStyleSheet("font: 14pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 214, 143);")
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.horizontalLayout_2.addWidget(self.pushButton_4)
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setStyleSheet("background-color: rgb(255, 214, 143);\n"
+"font: 14pt \"Microsoft YaHei\";\n"
+"")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout_2.addWidget(self.pushButton_3)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout_8 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_8.setObjectName("horizontalLayout_8")
+        self.verticalLayout.addLayout(self.horizontalLayout_8)
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setStyleSheet("font: 8pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 255, 205);")
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setStyleSheet("font: 18pt \"Microsoft YaHei\";")
+        self.label_4.setObjectName("label_4")
+        self.verticalLayout.addWidget(self.label_4)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setStyleSheet("font: 14pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 214, 143);")
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.horizontalLayout_3.addWidget(self.pushButton_7)
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setStyleSheet("font: 14pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 214, 143);")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.horizontalLayout_3.addWidget(self.pushButton_5)
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setStyleSheet("font: 12pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 255, 205);")
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.verticalLayout.addWidget(self.tableWidget)
+        self.horizontalLayout_4.addLayout(self.verticalLayout)
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout_3.addLayout(self.verticalLayout_2)
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setStyleSheet("font: 26pt \"Microsoft YaHei\";")
+        self.label_5.setObjectName("label_5")
+        self.verticalLayout_3.addWidget(self.label_5)
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setStyleSheet("background-color: rgb(255, 214, 143);\n"
+"font: 22pt \"Microsoft YaHei\";")
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.verticalLayout_3.addWidget(self.pushButton_6)
+        self.horizontalLayout_4.addLayout(self.verticalLayout_3)
+        self.verticalLayout_4.addLayout(self.horizontalLayout_4)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.label_2.setText(_translate("MainWindow", "TextLabel"))
+        self.label_3.setText(_translate("MainWindow", "TextLabel"))
+        self.pushButton.setText(_translate("MainWindow", "товар"))
+        self.pushButton_2.setText(_translate("MainWindow", "новый"))
+        self.pushButton_4.setText(_translate("MainWindow", "подбор"))
+        self.pushButton_3.setText(_translate("MainWindow", "доп окно"))
+        self.label_4.setText(_translate("MainWindow", "TextLabel"))
+        self.pushButton_7.setText(_translate("MainWindow", "отложить чек"))
+        self.pushButton_5.setText(_translate("MainWindow", "продолжить чек"))
+        self.label_5.setText(_translate("MainWindow", "TextLabel"))
+        self.pushButton_6.setText(_translate("MainWindow", "оплата"))
+
+
---
+++ design_okno_opl.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'design_okno_opl.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(804, 172)
+        Dialog.setStyleSheet("background-color: rgb(255, 255, 205);")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setStyleSheet("color:rgb(255, 30, 14);\n"
+"font: 18pt \"Microsoft YaHei\";")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setStyleSheet("font: 18pt \"Microsoft YaHei\";")
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setStyleSheet("font: 18pt \"Microsoft YaHei\";")
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout.addWidget(self.label_3)
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setHorizontalSpacing(0)
+        self.gridLayout.setVerticalSpacing(40)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setStyleSheet("font: 18pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 214, 143);")
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setStyleSheet("font: 18pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 214, 143);")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.gridLayout.addWidget(self.pushButton_2, 0, 1, 1, 1)
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setStyleSheet("font: 18pt \"Microsoft YaHei\";\n"
+"background-color: rgb(255, 214, 143);")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout.addWidget(self.pushButton_3, 0, 2, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton.setText(_translate("Dialog", "ОПЛАТА НАЛИЧНЫМИ"))
+        self.pushButton_2.setText(_translate("Dialog", "ОПЛАТА ПО КАРТЕ"))
+        self.pushButton_3.setText(_translate("Dialog", "СЛОЖНАЯ ОПЛАТА"))
+
+
